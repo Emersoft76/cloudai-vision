88c2e2df1467e6bd41f3229846ab8d20c94f2c7a
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2025-04-30 10:11:08 +0100

    main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "input" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "CloudAI Vision Input Bucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_dynamodb_table" "results" {
+  name           = var.dynamodb_table
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "imageKey"
+
+  attribute {
+    name = "imageKey"
+    type = "S"
+  }
+
+  tags = {
+    Name = "CloudAI Results Table"
+  }
+}
